51818ddc5d0d307a0c62101cc689ff560748b657
gondelsol <gondelsol@gmail.com>
2022-05-24 18:21:34 -0300

    finalizo vista del carro de compras

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -8,22 +8,18 @@ const ItemList = (item) => {
   return (
 
     <>
+    <div className="container d-flex justify-content-between flex-wrap">
 
 {
-          ensayos.map((item) => 
-          <div className="container d-flex justify-content-between flex-wrap">
-  <Item title={item.name} description={item.description} precio={item.precio} image={item.image}/>
+          ensayos.map((item) => (
+          <Item title={item.name} description={item.description} precio={item.precio} image={item.image}
+          />
 
-          </div>
-        
-
+          )
           )
         }
 
-
- 
-
-    
+</div>
     </>
 
   );
