2eba0f3de0f2f0eeb61ef2b8e67dff9f3a11a7b8
gondelsol <gondelsol@gmail.com>
2022-06-17 08:54:55 -0300

    firebase lectura de datos 2

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,4 +1,3 @@
-import customFetch from "../../utils/customFetch";
 import { useEffect, useState } from "react";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
@@ -7,56 +6,70 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 
 
 
+
+
+
 const ItemListContainer = () => {
     const [datos, setDatos]=useState([]);
     const {id} = useParams();
     const [cargando, setCargando] = useState(false);
 
+    const joa = [{
+        name: "joaquin",
+        stock:37},
+        {
+            name: "Karina",
+            stock: 38
+        }
+];
 
-    const mostrarTodosLosDatos = async () => {
-        const querySnapshot = await getDocs(collection(db, "Servicios"));
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-        });
+const mostrarTodosLosDatos = async () => {
+    const querySnapshot = await getDocs(collection(db, "Servicios"));
+    console.log("Todos los productos");
+    querySnapshot.forEach((doc) => {
 
-        return querySnapshot;
-    }
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+}
 
-    const filtrarDatos = async(id) => {
-        const q = query(collection(db, "Servicios"), where("categoryId", "==", parseInt(id)));
+const filtrarDatos = async(id) => {
+    const q = query(collection(db, "Servicios"), where("categoryId", "==", parseInt(id)));
+    const querySnapshot = await getDocs(q);
+    console.log("productos filtrados");
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+}
 
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-        });
 
-        return querySnapshot;
-
-    }
 
     useEffect(()=> {
 
 
+
         if ( id ==='1' || id ==='2') {
             setCargando(true);
-            customFetch (1000, filtrarDatos(id) )
+            filtrarDatos(id)
             .then(result => setDatos(result))
             .catch((err => console.log(err)))
             .finally(()=> setCargando(false))
 
         } else {
             setCargando(true);
-            customFetch (2000, mostrarTodosLosDatos())
+            mostrarTodosLosDatos()
             .then(result => setDatos(result))
-            .catch((err => console.log(err)))
+            .catch(err => console.log(err))
             .finally(()=> setCargando(false))
 
         }
 
+        console.log("ESTOS SON LOS DATOS DEL ITEMLISTCONTAINER");
+        console.log(datos);
 
-    }, [id]);
+
+    }, [id, datos]);
 
 
     return (
